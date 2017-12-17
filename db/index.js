17b8c5939dd6aa769e76e91987fd7df12330c985
tgwalker93 <tgwalker93@gmail.com>
2017-12-17 15:14:52 -0800

    changed db

--- db/index.js
+++ db/index.js
@@ -6,19 +6,20 @@ let MONGO_URL
 const MONGO_LOCAL_URL = 'mongodb://localhost/local-spectrum'
 
 
-// Connect to the Mongo DB
-// if (process.env.MONGODB_URI) {
-// 	mongoose.connect("mongodb://heroku_gvzh6nxl:752ndkfm90ojbhmsi5gmfeh9n1@ds133746.mlab.com:33746/heroku_gvzh6nxl")
-// 	MONGO_URL = process.env.MONGODB_URI
-// } else {
-// 	mongoose.connect(MONGO_LOCAL_URL) // local mongo url
-// 	MONGO_URL = MONGO_LOCAL_URL
-// }
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || MONGO_LOCAL_URL, {
-	useMongoClient: true
-});
+// Connect to the Mongo DB
+if (process.env.MONGODB_URI) {
+	mongoose.connect("mongodb://heroku_gvzh6nxl:752ndkfm90ojbhmsi5gmfeh9n1@ds133746.mlab.com:33746/heroku_gvzh6nxl")
+	MONGO_URL = process.env.MONGODB_URI
+} else {
+	mongoose.connect(MONGO_LOCAL_URL) // local mongo url
+	MONGO_URL = MONGO_LOCAL_URL
+}
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI || MONGO_LOCAL_URL, {
+// 	useMongoClient: true
+// });
 
 // should mongoose.connection be put in the call back of mongoose.connect???
 var db = mongoose.connection
