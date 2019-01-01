d19a0d9fe30f8129656a4de696ef6bffa5a5f744
tgwalker93 <tgwalker93@gmail.com>
2019-01-01 01:57:00 -0800

    added validation errors on register

--- client/src/components/Form/Input.js
+++ client/src/components/Form/Input.js
@@ -2,6 +2,8 @@ import React from "react";
 import 'font-awesome/css/font-awesome.min.css';
 
 export const Input = props =>
-  <div className="form-group">
-    <input className="form-control" {...props} />
+  <div className={props.formgroupclass}>
+
+    <input className="form-control inputLog" {...props} />
+    {props.isvalid === "true" ? "" : <span className="help-block">{props.fielderror}</span>}
   </div>;
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,5 +1,6 @@
 import { Row, Container } from "../../components/Grid";
 import { InputLog } from "../../components/LoginItem";
+import {Input } from "../../components/Form";
 
 
 import React, { Component } from "react";
@@ -37,7 +38,14 @@ class Register extends Component {
 			businessName: "",
 			zip: "",
 			confirmPassword: "",
-			redirectTo: null
+			redirectTo: null,
+			formErrors: { firstName: "", lastName: "", username: "", password: "", city: "", state:"" },
+			firstNameValid: false,
+			lastNameValid: false,
+			usernameValid: false,
+			passwordValid: false,
+			cityValid: false,
+			stateValid: false
 		}
 		this.handleSubmit = this.handleSubmit.bind(this)
 		this.handleChange = this.handleInputChange.bind(this)
@@ -51,7 +59,8 @@ class Register extends Component {
 	handleSubmit(event) {
 		event.preventDefault()
 		// TODO - validate!
-		if (this.state.username && this.state.password) {
+		
+		if (this.state.usernameValid && this.state.passwordValid && this.state.firstNameValid && this.state.lastNameValid && this.state.cityValid && this.state.stateValid) {
 		let userObj = {
 			userId: null,
 			username: this.state.username,
@@ -82,6 +91,61 @@ class Register extends Component {
 
 		}
 	}
+	//"has-error" is a default bootstrap class that will nicely color the outline of the field red to indicate an error for the user. 
+	errorClass(error) {
+		return (error.length === 0 ? "" : "has-error");
+	}
+
+	validateFields(event) {
+		event.preventDefault();
+		let fieldValidationErrors = this.state.formErrors;
+	
+		let firstNameValid = this.state.firstNameValid;
+		let lastNameValid = this.state.lastNameValid;
+		let usernameValid = this.state.usernameValid;
+		let passwordValid = this.state.passwordValid;
+		let cityValid = this.state.cityValid;
+		let stateValid = this.state.stateValid;
+
+
+
+
+		//Validating Fields by checking if there is anything there.
+		firstNameValid = this.state.firstName.length > 0;
+		fieldValidationErrors.firstName = firstNameValid ? "" : "Please provide your first name";
+
+		
+		lastNameValid = this.state.lastName.length > 0;
+		fieldValidationErrors.lastName = lastNameValid ? "" : "Please provide your last name";
+
+		usernameValid = this.state.username.length >0;
+		fieldValidationErrors.username = usernameValid ? "" : "Please provide your username";
+
+		passwordValid = this.state.password.length > 0;
+		fieldValidationErrors.password = passwordValid ? "" : "Please provide your password";
+
+		cityValid = this.state.city.length > 0;
+		fieldValidationErrors.city = cityValid ? "" : "Please provide your city";
+		
+		stateValid = this.state.state.length > 0;
+		fieldValidationErrors.state = stateValid ? "" : "Please provide your state";
+
+
+
+
+		this.setState({
+			formErrors: fieldValidationErrors,
+			firstNameValid: firstNameValid,
+			lastNameValid: lastNameValid,
+			usernameValid: usernameValid,
+			passwordValid: passwordValid,
+			cityValid: cityValid,
+			stateValid
+	
+		}, () => {
+			this.handleSubmit.bind(this);
+		});
+	}
 	render() {
 		if (this.state.redirectTo) {
 			return <Redirect to={{ pathname: this.state.redirectTo }} />
@@ -94,55 +158,49 @@ class Register extends Component {
 						<h1>{this.state.errorResponse} </h1> 
 						<div className="col-sm-3 hidden-xs"></div>
 						<div className="col-sm-6">
-
 							<form className="login">
+
 								<p>Username</p>
-								<InputLog
-									value={this.state.username}
-									onChange={this.handleInputChange}
-									name="username"
-									placeholder="&#xf007; USERNAME"
-								/>
+								<Input fielderror={this.state.formErrors.username}
+									formgroupclass={`form-group ${this.errorClass(this.state.formErrors.username)}`}
+									value={this.state.username} id="username"
+									onChange={this.handleInputChange.bind(this)}
+									name="username"></Input>
 								<p>Password</p>
-								<InputLog
-									value={this.state.password}
-									onChange={this.handleInputChange}
-									name="password"
-									type="password"
-									placeholder="&#xf023; PASSWORD"
-								/>
+								<Input fielderror={this.state.formErrors.password}
+									formgroupclass={`form-group ${this.errorClass(this.state.formErrors.password)}`}
+									value={this.state.password} id="password"
+									onChange={this.handleInputChange.bind(this)}
+									name="password"></Input>
 								<p>First Name</p>
-								<InputLog
-									value={this.state.firstName}
-									onChange={this.handleInputChange}
-									name="firstName"
-									placeholder="&#xf007; First Name"
-								/>
+								<Input fielderror={this.state.formErrors.firstName} 
+								formgroupclass={`form-group ${this.errorClass(this.state.formErrors.firstName)}`} 
+								value={this.state.firstName} id="firstName" 
+								onChange={this.handleInputChange.bind(this)} 
+								name="firstName"></Input>
+
 								<p>Last Name</p>
-								<InputLog
-									value={this.state.lastName}
-									onChange={this.handleInputChange}
-									name="lastName"
-									placeholder="&#xf007; Last Name"
-								/>
+								<Input fielderror={this.state.formErrors.lastName}
+									formgroupclass={`form-group ${this.errorClass(this.state.formErrors.lastName)}`}
+									value={this.state.lastName} id="lastName"
+									onChange={this.handleInputChange.bind(this)}
+									name="lastName"></Input>
 								<p> City</p>
-								<InputLog
-									value={this.state.city}
-									onChange={this.handleInputChange}
-									name="city"
-									placeholder="Irvine"
-								/>
+								<Input fielderror={this.state.formErrors.city}
+									formgroupclass={`form-group ${this.errorClass(this.state.formErrors.city)}`}
+									value={this.state.city} id="city"
+									onChange={this.handleInputChange.bind(this)}
+									name="city"></Input>
 								<p> State</p>
-								<InputLog
-									value={this.state.state}
-									onChange={this.handleInputChange}
-									name="state"
-									placeholder="California"
-								/>
+								<Input fielderror={this.state.formErrors.state}
+									formgroupclass={`form-group ${this.errorClass(this.state.formErrors.state)}`}
+									value={this.state.state} id="state"
+									onChange={this.handleInputChange.bind(this)}
+									name="state"></Input>
 
 
 								<button className="btn btn-warning registerBtn"
-									onClick={this.handleSubmit}
+									onClick={this.validateFields.bind(this)}
 								>
 									Register
                             </button>
